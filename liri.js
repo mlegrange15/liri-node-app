7313e46ff792e1b0534cc75d39dd35e7de672aef
Michael LeGrange <mlegrange15@att.net>
2018-08-04 15:17:50 -0500

    finished up the min req

--- liri.js
+++ liri.js
@@ -8,7 +8,7 @@ var keysINeed = require('./keys.js');
 var spotify = new Spotify(keysINeed.spotify);
 var client = new Twitter(keysINeed.twitter);
 var commandName = process.argv[2];
-var userRequest = process.argv[3];
+var userRequest = process.argv.slice(3).join(" ");
 
 
 // Function that list's of all of the possible commands Liri can accept and based on commandName & userRequest calls the right one.
@@ -38,40 +38,50 @@ function liriCommands(commandName, userRequest) {
 // SPOTIFY FUNCTION
 function spotifyThisSong(song) {
   spotify = new Spotify(keysINeed.spotify);
-
+  // If no song is input then do this
+  if (!song) {
+    song = "The Sign Ace";
+  };
+  // Spotify call
   spotify.search({ type: 'track', query: song }, function (err, data) {
     if (err) {
       console.log(err);
     }
-    console.log("The artist is: " + data.tracks.items[0].artists[0].name + "\nThe name of the song is: " + data.tracks.items[0].name + "\nHere is a preview link: " + data.tracks.items[0].preview_url + "\nThe name of the album is: " + data.tracks.items[0].album.name);
 
-    var logSong = "The artist is: " + data.tracks.items[0].artists[0].name + "\nThe name of the song is: " + data.tracks.items[0].name + "\nHere is a preview link: " + data.tracks.items[0].preview_url + "\nThe name of the album is: " + data.tracks.items[0].album.name;
+    console.log("\nThe artist is: " + data.tracks.items[0].artists[0].name +
+      "\nThe name of the song is: " + data.tracks.items[0].name +
+      "\nHere is a preview link: " + data.tracks.items[0].preview_url +
+      "\nThe name of the album is: " + data.tracks.items[0].album.name);
+
+    var logSong = "\nThe artist is: " + data.tracks.items[0].artists[0].name +
+      "\nThe name of the song is: " + data.tracks.items[0].name +
+      "\nHere is a preview link: " + data.tracks.items[0].preview_url +
+      "\nThe name of the album is: " + data.tracks.items[0].album.name + "\n";
 
     fs.appendFile('log.txt', logSong, function (err) {
       if (err) throw err;
     });
-
-    log(data);
   });
-
-  if (!song) {
-    song = "The Sign";
-  };
 };
-
 
 // TWITTER MY TWEETS FUNCTION
 function myTweets(handle) {
 
   client = new Twitter(keysINeed.twitter);
-
+  // Twitter call
   var params = { screen_name: handle, count: 20 };
   client.get('statuses/user_timeline', params, function (error, tweets, response) {
     if (!error) {
 
       for (i = 0; i < tweets.length; i++) {
 
-        console.log("Date: " + tweets[i].created_at + "\n --> " + tweets[i].text);
+        console.log("\nDate: " + tweets[i].created_at + "\n --> " + tweets[i].text + "\n");
+
+        var myTwitter = ("\nDate: " + tweets[i].created_at + "\n --> " + tweets[i].text + "\n");
+
+        fs.appendFile('log.txt', myTwitter, function (err) {
+          if (err) throw err;
+        });
       }
     }
   });
@@ -80,25 +90,40 @@ function myTweets(handle) {
 // MOVIE FUNCTION
 function movie(movie) {
 
-  var queryUrl = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
-
   if (!movie) {
     movie = "Mr Nobody";
   }
+  // OMDB call
+  var queryUrl = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
 
   request(queryUrl, function (error, response, body) {
 
     if (!error && response.statusCode === 200) {
 
-      console.log(JSON.parse(body));
-      console.log("Title: " + JSON.parse(body).Title +
-        "\nYear: " + JSON.parse(body).Year +
-        "\nIMDB Rating: " + JSON.parse(body).imdbRating +
-        "\nRotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value +
-        "\nCountry: " + JSON.parse(body).Country +
-        "\nLanguage: " + JSON.parse(body).Language +
-        "\nPlot: " + JSON.parse(body).Plot +
-        "\nActors: " + JSON.parse(body).Actors);
+      var data = JSON.parse(body);
+
+      console.log("\nTitle: " + data.Title +
+        "\nYear: " + data.Year +
+        "\nIMDB Rating: " + data.imdbRating +
+        // "\nRotten Tomatoes Rating: " + data.Ratings[1].Value +
+        "\nCountry: " + data.Country +
+        "\nLanguage: " + data.Language +
+        "\nPlot: " + data.Plot +
+        "\nActors: " + data.Actors);
+
+      var logMovie = "\nTitle: " + data.Title +
+        "\nYear: " + data.Year +
+        "\nIMDB Rating: " + data.imdbRating +
+        // "\nRotten Tomatoes Rating: " + data.Ratings[1].Value +
+        "\nCountry: " + data.Country +
+        "\nLanguage: " + data.Language +
+        "\nPlot: " + data.Plot +
+        "\nActors: " + data.Actors + "\n";
+
+      fs.appendFile('log.txt', logMovie, function (err) {
+        if (err) throw err;
+      });
+
     } else {
       console.log(error);
     }
@@ -107,7 +132,7 @@ function movie(movie) {
 
 // DO SOMETHING RANDOM FUNCTION
 function doRandom() {
-
+  // random.txt call
   fs.readFile("random.txt", "utf8", function (error, data) {
 
     var randomData = data.split(',');
@@ -116,26 +141,11 @@ function doRandom() {
       console.log(error);
 
     } else {
-      console.log(data);
-      console.log(randomData);
 
       liriCommands(randomData[0], randomData[1]);
     }
   });
 }
-
-
 // Calling function to accept user commands
 liriCommands(commandName, userRequest);
-
-// Sends the data we get back from our commands to the log.txt file
-function log(data) {
-  fs.appendFile("log.txt", data, function (err) {
-    if (err) {
-      console.log(err);
-    }
-    console.log("Sent");
-  });
-}
-
 
